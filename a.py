8efac639d1dda0387ad7d358b7c6afec176a21d0
zesankawsar <kashemshop@gmail.com>
2024-05-30 21:35:54 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,15 @@
+a = input("enter your name:")
+b = input("enter your gmail:")
+c = input("enter your password:")
+d = input("enter your user name:")
+print("your name is", a)
+print("your gmail is", b)
+print("your password is", c)
+print("your  is user name", d)
+
+
+
+x = input("enter your num:")
+y = input("enter your num:")
+print(x + y)
+print(int(x) + int(y))
