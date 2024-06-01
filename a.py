1f0f3d10caecc052f9db0d698d506299a5ff78ac
zesankawsar <kashemshop@gmail.com>
2024-06-01 21:47:29 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,11 @@
+Names = "kawsar,ankon"
+print(Names[0:12])
+kawsar = "ajdmklamknkmnleajkrtgnjmernklmklaaaad;def,lael,frelkmkmlerfg,ae,l,lra,lrle;,,lr;,lalrelremrmkamkllfralaerl;tr,pra;,a;,,l;lrmlaa,la,fal;ka,l;l,;aelahgfmn   ffg"
+print(len(kawsar))
+fruit = "apple,mango,lichi"
+summur = len(fruit)
+print("apple,mango and lichi is a", summur ,
+"letter word. ")
+# quick quiz
+nm = "zesan"
+print(nm[-4:-2])
