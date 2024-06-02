7550cbb50fe5758e53675194502147888daadc0d
zesankawsar <kashemshop@gmail.com>
2024-06-02 22:44:39 +0600

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,20 @@
+a = " kawsar zehan "
+s = 'KAWSAR///'
+k = "shark tank"
+print(s.lower())
+print(len(a)) 
+print(a.upper())  
+print(s.rstrip("/"))
+print(k.replace("shark tank" , "cring rakib hossain"))
+print(a.split(" "))
+blogheading = "introduction  to blood stone Smp In MInecraft"
+print(blogheading.capitalize())
+f2 = "welcome to the new minecraft smp which is free kindom"
+print(f2.center(90))
+print(k.count("a")) 
+f1 = "Welcome To The Console !!!"
+print(f1.endswith("!!!"))
+st1 ="          "       #using space
+print(st1.isspace())
+st2 = "                 "   #using tab
+print(st2.isspace())
