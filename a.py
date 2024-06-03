7c408fd1fa396c77b1ea689ca7a33e3a635e4f7a
zesankawsar <kashemshop@gmail.com>
2024-06-03 22:38:29 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+a = int(input("Enter your class:"))
+print("Your class is ", a)
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if(a>18):
+    print("You can drive")
+else:
+    print("You can not drive")
+appleprice = 190
+budget = 90
+if(appleprice <= budget):
+    print("Alexa, add 1 kg apples to the card.")
+
+else:
+    print("Alexa, do not add apple to the card.")
