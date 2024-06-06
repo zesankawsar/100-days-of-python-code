de0eb530f7dea06a7a4bfa5e6aa096ca89820e01
zesankawsar <kashemshop@gmail.com>
2024-06-06 20:50:05 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+name = "Zesan"
+for i in name:
+    print(i)
+    if (i =="b"):
+        print("this is something special")
+colours = ["Red","Black","Green","Brown","Blue","white"]
+for i in colours:
+    print(i)    
+for k in range(13):
+        print(k)
