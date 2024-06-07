743b1cf60eac4edf0f3fa5d8f317535ac3a122ae
zesankawsar <kashemshop@gmail.com>
2024-06-07 21:14:10 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+i = int(input("Enter the number:"))
+while(i<=38):
+    i = int(input("Enter the number:"))
+print(i)
+print("Done with the loop32")
+
+count = 5
+while (count > 0):
+    print(count)
+    count = count -2
