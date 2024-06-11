a8e6a00ab595c2a105bb620c96e6060e1a52698b
zesankawsar <kashemshop@gmail.com>
2024-06-11 22:40:48 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,13 @@
+list = [2,4,4,65,121,32,1]
+# print(list)
+# print(type(list))
+# print(list[0])
+# print(list[1])
+# # print(list[len(list)-2])
+# if "642" in "121":
+#     print("Yes")
+# else:
+#     print("No")
+print(list[1:8:3])
+lst = [ok*ok for ok in range(11)]
+print(lst)
