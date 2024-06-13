0537ce8f3e84a63adc33e4bdca31cc0a75f5a502
zesankawsar <kashemshop@gmail.com>
2024-06-13 21:00:46 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+tuple = (1,2,3,42,32 ,43 ,35,32,32)
+print(type(tuple), tuple)
+print(len(tuple))
+print(tuple[0])
+print(tuple[2])
+print(tuple[3])
+if   32 in tuple:
+    print("yes")
+tup2 = tuple[1:7]
