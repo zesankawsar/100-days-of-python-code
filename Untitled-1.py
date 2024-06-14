71ee1684d3d6669ecb06bec40bbbc65867f7f2b7
zesankawsar <kashemshop@gmail.com>
2024-06-14 21:25:48 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+countres = ("pakistan","Bagladesh","spain","indea")
+countres2 = ("viatnum","chaina")
+southeastasia = countres+countres2
+print(southeastasia)
+
+tuple1 = (0,1,2,3,3,6,4,3,21,4,)
+res = tuple1.index(3, 4, 8)
+print(res)
