ccf09d276102ca4ea4cb40dce2399e28ccc3eb96
zesankawsar <kashemshop@gmail.com>
2024-06-16 17:28:05 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+print("Wellcome to kon danega karorpati show\nif you ansear 3 qustion you win 70000000 taka")
+Round1 = (input("How Many people live in Bangladesh:"))
+if  (Round1 == 169,828,911):
+    print('congratulation your first anser is currect\nYour point is 1')
+else:
+    print("Your anser is wrong\nyour point is -1")
+Round2 = (input("In what year did Bangladesh get independence:"))
+if  (Round2 == 1971):
+    print("congratulation your anser is currect\nand Your point is 2")
+else:
+    print("Your anser is wrong\nyour point is -2")
+Round3 =(input("Which year Bangobondu Death:"))
+if (Round3 == 1975):
+    print("Congratulation your anser is currect\nYour point is 3\nYou Win 70000000")
+else:
+    print("Your anser is wrong\nyou Don't win 70000000 taka")
+print('Thanks for playing the game')
