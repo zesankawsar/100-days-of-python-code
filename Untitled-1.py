d73d1b1a9e7220e301775ff7533636ab7d864f30
zesankawsar <kashemshop@gmail.com>
2024-06-19 18:51:11 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+latter = "hey my name is {1} and iam from{0}"
+country = "Bangladesh"
+name = "Zesan"
+print(latter.format(country ,name))
+prize = 42032
+txt = f"for only {prize:2f} dollers"
+print(txt)
