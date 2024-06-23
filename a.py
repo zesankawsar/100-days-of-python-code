2441d6c44cb6281e4672cc49c1567e244e6e9de8
zesankawsar <kashemshop@gmail.com>
2024-06-23 21:00:59 +0600

    Add files via upload

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
-def factorial (n):
-    if (n==0 or n==1 ):
-        return 1
-    else:
-        return n * factorial(n-1)
-print(factorial(32))
+set1 = {1, 3, 3, 4, 3, 4}
+set2 = { 21, 21, 21, 3 ,2}
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.symmetric_difference(set2))
+print(set1, set2)
