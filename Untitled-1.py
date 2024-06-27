c51eeb796a68a70624488b7acee995ff7a6248bb
zesankawsar <kashemshop@gmail.com>
2024-06-27 21:26:05 +0600

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,10 @@
-EP1 = {122:31, 123:32, 1002:90,  120:70, }
-EP2 = {2313:34, 92:21}
-EP2.update(EP1)
-# EP2.clear(EP1)
-# EP1.popitem(EP2)
-print(EP2)
-print(EP1)
+a = input("Enter the number:")
+print(f"multypication table of {a} is")
+try:
+    for i in range(1, 11):
+        print(f"{int(a)} X {i} = {int(a*i)}")
+except ValueError:
+    print("error 404 not found")
+
+
+print("End of program")
