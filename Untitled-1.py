699dd1374d3c165af8592bbfd230d6b647f8d714
zesankawsar <kashemshop@gmail.com>
2024-06-29 22:51:37 +0600

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,3 @@
-def fun1():
-    try:
-        l = [1, 22, 72, 871, 17, 817]
-        i = int(input("Enter the index"))
-        print(l)
-        print(i)
-    except:
-        print("something wrong")
-    finally:
-        print("iam always executed")
-x = fun1
-print(x)
+a = int(input("Enter a number beetwen 1-10 "))
+if (a<1 or a<10):
+    raise ValueError("Value should be between 1 and 10")
