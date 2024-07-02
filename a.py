c72121c1dbb58ff224fa07ae29aecdf25c977e15
zesankawsar <kashemshop@gmail.com>
2024-07-02 20:57:20 +0600

    Add files via upload

--- a.py
+++ a.py
@@ -1,10 +1,4 @@
-for i in range(6):
-    print(i)
-    if i == 4:
-        break
-    print("sorry no i")
-for x in range(5):
-        print ("iteration no {} in for loop".format(x+1))
-else:
-    print("else block in loop")
-print("out of loop")    
+a = (input("Enter fist word of code:"))
+b = (input("Enter your full word:"))
+s = f'your secret code is {b}{a}' 
+print(s)
